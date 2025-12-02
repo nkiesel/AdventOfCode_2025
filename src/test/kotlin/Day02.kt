b2e4151cef4a59dc9e368f9ea60f9de4837c575c
Norbert Kiesel <nk@iname.com>
2025-12-01 21:36:30 -0800

    Day02

---
+++ src/test/kotlin/Day02.kt
@@ -0,0 +1,38 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+object Day02 {
+    private fun parse(input: List<String>) =
+        input[0].split(',').map { it.split('-').map { it.toLong() }.let { it[0]..it[1] } }
+
+    fun one(input: List<String>): Long = three(input, Regex("""(\d+)\1"""))
+
+    fun two(input: List<String>): Long = three(input, Regex("""(\d+)\1+"""))
+
+    private fun three(input: List<String>, rx: Regex) =
+        parse(input).flatMap { r -> r.filter { rx.matches(it.toString()) } }.sum()
+}
+
+object Day02Test : FunSpec({
+    val input = lines("Day02")
+
+    val sample = """
+        11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+    """.trimIndent().lines()
+
+    with(Day02) {
+        test("one") {
+            one(sample) shouldBe 1227775554L
+            one(input) shouldBe 54234399924L
+        }
+
+        test("two") {
+            two(sample) shouldBe 4174379265L
+            two(input) shouldBe 70187097315L
+        }
+    }
+})
+
+/*
+Much simpler than Day 1!!! Regular expressions are such a powerful helper!
+*/
