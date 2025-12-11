2ec728cfdd42710b5c45afcd32e62d2aa344a864
Norbert Kiesel <nk@iname.com>
2025-12-10 23:21:30 -0800

    Day11

---
+++ src/test/kotlin/Day11.kt
@@ -0,0 +1,101 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+object Day11 {
+    private fun parse(input: List<String>) =
+        input.associate { it.substringBefore(":") to it.substringAfter(": ").split(" ") }
+
+    fun one(input: List<String>): Int {
+        val devs = parse(input)
+        val paths = mutableListOf<List<String>>()
+        paths.add(mutableListOf("you"))
+        while (true) {
+            val next = paths.filter { it.last() != "out" }
+            if (next.isEmpty()) return paths.size
+            for (n in next) {
+                paths.remove(n)
+                devs[n.last()]!!.forEach { p -> paths.add(n + p) }
+            }
+        }
+    }
+
+    fun oneA(input: List<String>): Long = with(parse(input)) { return count("you", "out") }
+
+    private data class Path(val dev: String, val count: Long)
+
+    context(devs: Map<String, List<String>>)
+    private fun count(start: String, end: String): Long {
+        var paths = listOf(Path(start, 1))
+        var count = 0L
+        while (true) {
+            val next = buildList {
+                for (n in paths) {
+                    for (p in devs[n.dev].orEmpty()) {
+                        if (p == end) count += n.count else add(Path(p, n.count))
+                    }
+                }
+            }
+            if (next.isEmpty()) return count
+            paths = next.groupBy { it.dev }.map { (dev, counts) -> Path(dev, counts.sumOf { it.count }) }
+        }
+    }
+
+    fun two(input: List<String>): Long = with(parse(input)) {
+        return count("svr", "dac") * count("dac", "fft") * count("fft", "out") +
+                count("svr", "fft") * count("fft", "dac") * count("dac", "out")
+    }
+}
+
+object Day11Test : FunSpec({
+    val input = lines("Day11")
+
+    val sample = """
+        aaa: you hhh
+        you: bbb ccc
+        bbb: ddd eee
+        ccc: ddd eee fff
+        ddd: ggg
+        eee: out
+        fff: out
+        ggg: out
+        hhh: ccc fff iii
+        iii: out
+    """.trimIndent().lines()
+
+    val sample2 = """
+        svr: aaa bbb
+        aaa: fft
+        fft: ccc
+        bbb: tty
+        tty: ccc
+        ccc: ddd eee
+        ddd: hub
+        hub: fff
+        eee: dac
+        dac: fff
+        fff: ggg hhh
+        ggg: out
+        hhh: out
+    """.trimIndent().lines()
+
+    with(Day11) {
+        test("one") {
+            one(sample) shouldBe 5
+            one(input) shouldBe 719
+            oneA(input) shouldBe 719L
+        }
+
+        test("two") {
+            two(sample2) shouldBe 2L
+            two(input) shouldBe 337433554149492L
+        }
+    }
+})
+
+/*
+This was nice!  Part 1 was very straight forward.  For part 2, I first thought that breaking the paths into
+multiple groups would be sufficient, but that did not work.  I still thought I would have to avoid circles,
+but I never saw any while my part 2 code was building paths similar to part 1.  I then decided to try coding
+with the assumption that there would be no circles, and that worked.  oneA is therefore a better solution than
+one for part 1.
+ */
